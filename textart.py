d21768b4a9688970e4acbeb21568aacd5a6390a3
navjack <jack.mangano@gmail.com>
2022-02-02 20:48:19 -0500

    Create textart.py

---
+++ textart.py
@@ -0,0 +1,382 @@
+text_to_transform_1 = ""
+text_to_transform_2 = ""
+text_to_transform_3 = ""
+text_to_transform_4 = ""
+text_to_transform_5 = ""
+text_to_transform_6 = ""
+text_to_transform = "hi jack"
+transformed_text = ""
+CAPS_LETTER_A_1 = "   ▐██▌   "
+CAPS_LETTER_A_2 = "   █▀▀█   "
+CAPS_LETTER_A_3 = "   █▄▄█   "
+CAPS_LETTER_A_4 = "  ▐█  █▌  "
+CAPS_LETTER_A_5 = "  █▌  ▐█  "
+CAPS_LETTER_A_6 = " ▐█    █▌ "
+CAPS_LETTER_B_1 = " ▐▀▀▀▀▀▄  "
+CAPS_LETTER_B_2 = " ▐      █ "
+CAPS_LETTER_B_3 = " ▐█████▀  "
+CAPS_LETTER_B_4 = " ▐    ▀▄  "
+CAPS_LETTER_B_5 = " ▐      █ "
+CAPS_LETTER_B_6 = " ▐█████▀  "
+CAPS_LETTER_C_1 = "  ▄█▀▀▀▀▀ "
+CAPS_LETTER_C_2 = " █▀       "
+CAPS_LETTER_C_3 = " █        "
+CAPS_LETTER_C_4 = " █        "
+CAPS_LETTER_C_5 = " █▄       "
+CAPS_LETTER_C_6 = "  ▀█▄▄▄▄▄ "
+CAPS_LETTER_D_1 = " █▀▀▀▀▀█  "
+CAPS_LETTER_D_2 = " █      █ "
+CAPS_LETTER_D_3 = " █      █ "
+CAPS_LETTER_D_4 = " █      █ "
+CAPS_LETTER_D_5 = " █     █  "
+CAPS_LETTER_D_6 = " ██████   "
+CAPS_LETTER_E_1 = " █▀▀▀▀▀▀█ "
+CAPS_LETTER_E_2 = " █        "
+CAPS_LETTER_E_3 = " █▄▄▄▄▄█  "
+CAPS_LETTER_E_4 = " █▀▀▀▀▀█  "
+CAPS_LETTER_E_5 = " █        "
+CAPS_LETTER_E_6 = " █▄▄▄▄▄▄█ "
+CAPS_LETTER_F_1 = " █▀▀▀▀▀▀█ "
+CAPS_LETTER_F_2 = " █        "
+CAPS_LETTER_F_3 = " █▄▄▄▄▄   "
+CAPS_LETTER_F_4 = " █        "
+CAPS_LETTER_F_5 = " █        "
+CAPS_LETTER_F_6 = " █▄       "
+CAPS_LETTER_G_1 = "  ▄▀▀▀▀▀█ "
+CAPS_LETTER_G_2 = " █        "
+CAPS_LETTER_G_3 = " █        "
+CAPS_LETTER_G_4 = " █   ▄▄▄▄ "
+CAPS_LETTER_G_5 = " █    ▀▀█ "
+CAPS_LETTER_G_6 = "  ▀▄▄▄▄▄█ "
+CAPS_LETTER_H_1 = " ▀▄    ▄▀ "
+CAPS_LETTER_H_2 = "  █    █  "
+CAPS_LETTER_H_3 = "  █▄▄▄▄█  "
+CAPS_LETTER_H_4 = "  █▀▀▀▀█  "
+CAPS_LETTER_H_5 = "  █    █  "
+CAPS_LETTER_H_6 = " ▄▀    ▀▄ "
+CAPS_LETTER_I_1 = " █▀▀██▀▀█ "
+CAPS_LETTER_I_2 = "    ██    "
+CAPS_LETTER_I_3 = "    ▐▌    "
+CAPS_LETTER_I_4 = "    ▐▌    "
+CAPS_LETTER_I_5 = "    ██    "
+CAPS_LETTER_I_6 = " █▄▄██▄▄█ "
+CAPS_LETTER_J_1 = " █▀▀██▀▀█ "
+CAPS_LETTER_J_2 = "     █    "
+CAPS_LETTER_J_3 = "     ▐    "
+CAPS_LETTER_J_4 = "     ▐    "
+CAPS_LETTER_J_5 = " ▄▀  █    "
+CAPS_LETTER_J_6 = " ▀▄▄█▌    "
+CAPS_LETTER_K_1 = " █    ▄█▀ "
+CAPS_LETTER_K_2 = " █  ▄█▀   "
+CAPS_LETTER_K_3 = " ███▀     "
+CAPS_LETTER_K_4 = " ███▄     "
+CAPS_LETTER_K_5 = " █  ▀█▄   "
+CAPS_LETTER_K_6 = " █    ▀█▄ "
+CAPS_LETTER_L_1 = " █        "
+CAPS_LETTER_L_2 = " █        "
+CAPS_LETTER_L_3 = " █        "
+CAPS_LETTER_L_4 = " █        "
+CAPS_LETTER_L_5 = " █        "
+CAPS_LETTER_L_6 = " ▀▄▄▄▄▄▄█ "
+CAPS_LETTER_M_1 = " █      █ "
+CAPS_LETTER_M_2 = " ██    ██ "
+CAPS_LETTER_M_3 = " █ █  █ █ "
+CAPS_LETTER_M_4 = " █  ██  █ "
+CAPS_LETTER_M_5 = " █      █ "
+CAPS_LETTER_M_6 = " █      █ "
+CAPS_LETTER_N_1 = " █      █ "
+CAPS_LETTER_N_2 = " ██     █ "
+CAPS_LETTER_N_3 = " █ ██   █ "
+CAPS_LETTER_N_4 = " █   ██ █ "
+CAPS_LETTER_N_5 = " █     ██ "
+CAPS_LETTER_N_6 = " █      █ "
+CAPS_LETTER_O_1 = "   ▄██▄   "
+CAPS_LETTER_O_2 = " ▄█▀  ▀█▄ "
+CAPS_LETTER_O_3 = " █      █ "
+CAPS_LETTER_O_4 = " █      █ "
+CAPS_LETTER_O_5 = " ▀█▄  ▄█▀ "
+CAPS_LETTER_O_6 = "   ▀██▀   "
+CAPS_LETTER_P_1 = " █▀▀▀▀▀▄  "
+CAPS_LETTER_P_2 = " █      █ "
+CAPS_LETTER_P_3 = " █      █ "
+CAPS_LETTER_P_4 = " █▄▄▄▄▄▀  "
+CAPS_LETTER_P_5 = " █        "
+CAPS_LETTER_P_6 = " █        "
+CAPS_LETTER_Q_1 = "   ▄██▄   "
+CAPS_LETTER_Q_2 = " ▄█▀  ▀█▄ "
+CAPS_LETTER_Q_3 = " █      █ "
+CAPS_LETTER_Q_4 = " █   ▄  █ "
+CAPS_LETTER_Q_5 = " ▀█▄ ▀██▀ "
+CAPS_LETTER_Q_6 = "   ▀██▀▀█▄"
+CAPS_LETTER_R_1 = " █▀▀▀▀▀▄  "
+CAPS_LETTER_R_2 = " █      █ "
+CAPS_LETTER_R_3 = " █      █ "
+CAPS_LETTER_R_4 = " █▄▄▄▄▄▀  "
+CAPS_LETTER_R_5 = " █     ▀▄ "
+CAPS_LETTER_R_6 = " █      █ "
+CAPS_LETTER_S_1 = "   ▄▄▀▀▀▀ "
+CAPS_LETTER_S_2 = " ▄▀       "
+CAPS_LETTER_S_3 = " ▀▄▄▄▄▄   "
+CAPS_LETTER_S_4 = "   ▀▀▀▀▀▄ "
+CAPS_LETTER_S_5 = "       ▄▀ "
+CAPS_LETTER_S_6 = " ▄▄▄▄▀▀   "
+CAPS_LETTER_T_1 = " █▀████▀█ "
+CAPS_LETTER_T_2 = "    ██    "
+CAPS_LETTER_T_3 = "    ██    "
+CAPS_LETTER_T_4 = "    ▐▌    "
+CAPS_LETTER_T_5 = "    ▐▌    "
+CAPS_LETTER_T_6 = "    ▐▌    "
+CAPS_LETTER_U_1 = " ▌      ▐ "
+CAPS_LETTER_U_2 = " █      █ "
+CAPS_LETTER_U_3 = " █      █ "
+CAPS_LETTER_U_4 = " █      █ "
+CAPS_LETTER_U_5 = " █▄    ▄█ "
+CAPS_LETTER_U_6 = " ████████ "
+CAPS_LETTER_V_1 = " █      █ "
+CAPS_LETTER_V_2 = " █      █ "
+CAPS_LETTER_V_3 = " █      █ "
+CAPS_LETTER_V_4 = " █      █ "
+CAPS_LETTER_V_5 = "  █    █  "
+CAPS_LETTER_V_6 = "   ▀▄▄▀   "
+CAPS_LETTER_W_1 = "▐▌      ▐▌"
+CAPS_LETTER_W_2 = " █      █ "
+CAPS_LETTER_W_3 = " ▐  ▐▌  ▌ "
+CAPS_LETTER_W_4 = "  ▌▐▌▐▌▐  "
+CAPS_LETTER_W_5 = "  ▌▌  ▐▐  "
+CAPS_LETTER_W_6 = "  █    █  "
+CAPS_LETTER_X_1 = "█▄      ▄█"
+CAPS_LETTER_X_2 = " ▀█▄  ▄█▀ "
+CAPS_LETTER_X_3 = "   ▀██▀   "
+CAPS_LETTER_X_4 = "   ▄██▄   "
+CAPS_LETTER_X_5 = " ▄█▀  ▀█▄ "
+CAPS_LETTER_X_6 = "█▀      ▀█"
+CAPS_LETTER_Y_1 = " █      █ "
+CAPS_LETTER_Y_2 = " ▀▄    ▄▀ "
+CAPS_LETTER_Y_3 = "   ▀▄▄▀   "
+CAPS_LETTER_Y_4 = "    ▐▌    "
+CAPS_LETTER_Y_5 = "    ██    "
+CAPS_LETTER_Y_6 = "    ██    "
+CAPS_LETTER_Z_1 = "█▀▀▀▀▀▀▀▀█"
+CAPS_LETTER_Z_2 = "       ▄▀ "
+CAPS_LETTER_Z_3 = "     ▄▀   "
+CAPS_LETTER_Z_4 = "   ▄▀     "
+CAPS_LETTER_Z_5 = " ▄▀       "
+CAPS_LETTER_Z_6 = "█▄▄▄▄▄▄▄▄█"
+
+text_to_transform = input("What text would you like to transform?\n")
+
+text_to_transform = text_to_transform.upper()
+
+for letter in text_to_transform:
+    if letter == "A":
+        text_to_transform_1 += CAPS_LETTER_A_1
+        text_to_transform_2 += CAPS_LETTER_A_2
+        text_to_transform_3 += CAPS_LETTER_A_3
+        text_to_transform_4 += CAPS_LETTER_A_4
+        text_to_transform_5 += CAPS_LETTER_A_5
+        text_to_transform_6 += CAPS_LETTER_A_6
+    elif letter == "B":
+        text_to_transform_1 += CAPS_LETTER_B_1
+        text_to_transform_2 += CAPS_LETTER_B_2
+        text_to_transform_3 += CAPS_LETTER_B_3
+        text_to_transform_4 += CAPS_LETTER_B_4
+        text_to_transform_5 += CAPS_LETTER_B_5
+        text_to_transform_6 += CAPS_LETTER_B_6
+    elif letter == "C":
+        text_to_transform_1 += CAPS_LETTER_C_1
+        text_to_transform_2 += CAPS_LETTER_C_2
+        text_to_transform_3 += CAPS_LETTER_C_3
+        text_to_transform_4 += CAPS_LETTER_C_4
+        text_to_transform_5 += CAPS_LETTER_C_5
+        text_to_transform_6 += CAPS_LETTER_C_6
+    elif letter == "D":
+        text_to_transform_1 += CAPS_LETTER_D_1
+        text_to_transform_2 += CAPS_LETTER_D_2
+        text_to_transform_3 += CAPS_LETTER_D_3
+        text_to_transform_4 += CAPS_LETTER_D_4
+        text_to_transform_5 += CAPS_LETTER_D_5
+        text_to_transform_6 += CAPS_LETTER_D_6
+    elif letter == "E":
+        text_to_transform_1 += CAPS_LETTER_E_1
+        text_to_transform_2 += CAPS_LETTER_E_2
+        text_to_transform_3 += CAPS_LETTER_E_3
+        text_to_transform_4 += CAPS_LETTER_E_4
+        text_to_transform_5 += CAPS_LETTER_E_5
+        text_to_transform_6 += CAPS_LETTER_E_6
+    elif letter == "F":
+        text_to_transform_1 += CAPS_LETTER_F_1
+        text_to_transform_2 += CAPS_LETTER_F_2
+        text_to_transform_3 += CAPS_LETTER_F_3
+        text_to_transform_4 += CAPS_LETTER_F_4
+        text_to_transform_5 += CAPS_LETTER_F_5
+        text_to_transform_6 += CAPS_LETTER_F_6
+    elif letter == "G":
+        text_to_transform_1 += CAPS_LETTER_G_1
+        text_to_transform_2 += CAPS_LETTER_G_2
+        text_to_transform_3 += CAPS_LETTER_G_3
+        text_to_transform_4 += CAPS_LETTER_G_4
+        text_to_transform_5 += CAPS_LETTER_G_5
+        text_to_transform_6 += CAPS_LETTER_G_6
+    elif letter == "H":
+        text_to_transform_1 += CAPS_LETTER_H_1
+        text_to_transform_2 += CAPS_LETTER_H_2
+        text_to_transform_3 += CAPS_LETTER_H_3
+        text_to_transform_4 += CAPS_LETTER_H_4
+        text_to_transform_5 += CAPS_LETTER_H_5
+        text_to_transform_6 += CAPS_LETTER_H_6
+    elif letter == "I":
+        text_to_transform_1 += CAPS_LETTER_I_1
+        text_to_transform_2 += CAPS_LETTER_I_2
+        text_to_transform_3 += CAPS_LETTER_I_3
+        text_to_transform_4 += CAPS_LETTER_I_4
+        text_to_transform_5 += CAPS_LETTER_I_5
+        text_to_transform_6 += CAPS_LETTER_I_6
+    elif letter == "J":
+        text_to_transform_1 += CAPS_LETTER_J_1
+        text_to_transform_2 += CAPS_LETTER_J_2
+        text_to_transform_3 += CAPS_LETTER_J_3
+        text_to_transform_4 += CAPS_LETTER_J_4
+        text_to_transform_5 += CAPS_LETTER_J_5
+        text_to_transform_6 += CAPS_LETTER_J_6
+    elif letter == "K":
+        text_to_transform_1 += CAPS_LETTER_K_1
+        text_to_transform_2 += CAPS_LETTER_K_2
+        text_to_transform_3 += CAPS_LETTER_K_3
+        text_to_transform_4 += CAPS_LETTER_K_4
+        text_to_transform_5 += CAPS_LETTER_K_5
+        text_to_transform_6 += CAPS_LETTER_K_6
+    elif letter == "L":
+        text_to_transform_1 += CAPS_LETTER_L_1
+        text_to_transform_2 += CAPS_LETTER_L_2
+        text_to_transform_3 += CAPS_LETTER_L_3
+        text_to_transform_4 += CAPS_LETTER_L_4
+        text_to_transform_5 += CAPS_LETTER_L_5
+        text_to_transform_6 += CAPS_LETTER_L_6
+    elif letter == "M":
+        text_to_transform_1 += CAPS_LETTER_M_1
+        text_to_transform_2 += CAPS_LETTER_M_2
+        text_to_transform_3 += CAPS_LETTER_M_3
+        text_to_transform_4 += CAPS_LETTER_M_4
+        text_to_transform_5 += CAPS_LETTER_M_5
+        text_to_transform_6 += CAPS_LETTER_M_6
+    elif letter == "N":
+        text_to_transform_1 += CAPS_LETTER_N_1
+        text_to_transform_2 += CAPS_LETTER_N_2
+        text_to_transform_3 += CAPS_LETTER_N_3
+        text_to_transform_4 += CAPS_LETTER_N_4
+        text_to_transform_5 += CAPS_LETTER_N_5
+        text_to_transform_6 += CAPS_LETTER_N_6
+    elif letter == "O":
+        text_to_transform_1 += CAPS_LETTER_O_1
+        text_to_transform_2 += CAPS_LETTER_O_2
+        text_to_transform_3 += CAPS_LETTER_O_3
+        text_to_transform_4 += CAPS_LETTER_O_4
+        text_to_transform_5 += CAPS_LETTER_O_5
+        text_to_transform_6 += CAPS_LETTER_O_6
+    elif letter == "P":
+        text_to_transform_1 += CAPS_LETTER_P_1
+        text_to_transform_2 += CAPS_LETTER_P_2
+        text_to_transform_3 += CAPS_LETTER_P_3
+        text_to_transform_4 += CAPS_LETTER_P_4
+        text_to_transform_5 += CAPS_LETTER_P_5
+        text_to_transform_6 += CAPS_LETTER_P_6
+    elif letter == "Q":
+        text_to_transform_1 += CAPS_LETTER_Q_1
+        text_to_transform_2 += CAPS_LETTER_Q_2
+        text_to_transform_3 += CAPS_LETTER_Q_3
+        text_to_transform_4 += CAPS_LETTER_Q_4
+        text_to_transform_5 += CAPS_LETTER_Q_5
+        text_to_transform_6 += CAPS_LETTER_Q_6
+    elif letter == "R":
+        text_to_transform_1 += CAPS_LETTER_R_1
+        text_to_transform_2 += CAPS_LETTER_R_2
+        text_to_transform_3 += CAPS_LETTER_R_3
+        text_to_transform_4 += CAPS_LETTER_R_4
+        text_to_transform_5 += CAPS_LETTER_R_5
+        text_to_transform_6 += CAPS_LETTER_R_6
+    elif letter == "S":
+        text_to_transform_1 += CAPS_LETTER_S_1
+        text_to_transform_2 += CAPS_LETTER_S_2
+        text_to_transform_3 += CAPS_LETTER_S_3
+        text_to_transform_4 += CAPS_LETTER_S_4
+        text_to_transform_5 += CAPS_LETTER_S_5
+        text_to_transform_6 += CAPS_LETTER_S_6
+    elif letter == "T":
+        text_to_transform_1 += CAPS_LETTER_T_1
+        text_to_transform_2 += CAPS_LETTER_T_2
+        text_to_transform_3 += CAPS_LETTER_T_3
+        text_to_transform_4 += CAPS_LETTER_T_4
+        text_to_transform_5 += CAPS_LETTER_T_5
+        text_to_transform_6 += CAPS_LETTER_T_6
+    elif letter == "U":
+        text_to_transform_1 += CAPS_LETTER_U_1
+        text_to_transform_2 += CAPS_LETTER_U_2
+        text_to_transform_3 += CAPS_LETTER_U_3
+        text_to_transform_4 += CAPS_LETTER_U_4
+        text_to_transform_5 += CAPS_LETTER_U_5
+        text_to_transform_6 += CAPS_LETTER_U_6
+    elif letter == "V":
+        text_to_transform_1 += CAPS_LETTER_V_1
+        text_to_transform_2 += CAPS_LETTER_V_2
+        text_to_transform_3 += CAPS_LETTER_V_3
+        text_to_transform_4 += CAPS_LETTER_V_4
+        text_to_transform_5 += CAPS_LETTER_V_5
+        text_to_transform_6 += CAPS_LETTER_V_6
+    elif letter == "W":
+        text_to_transform_1 += CAPS_LETTER_W_1
+        text_to_transform_2 += CAPS_LETTER_W_2
+        text_to_transform_3 += CAPS_LETTER_W_3
+        text_to_transform_4 += CAPS_LETTER_W_4
+        text_to_transform_5 += CAPS_LETTER_W_5
+        text_to_transform_6 += CAPS_LETTER_W_6
+    elif letter == "X":
+        text_to_transform_1 += CAPS_LETTER_X_1
+        text_to_transform_2 += CAPS_LETTER_X_2
+        text_to_transform_3 += CAPS_LETTER_X_3
+        text_to_transform_4 += CAPS_LETTER_X_4
+        text_to_transform_5 += CAPS_LETTER_X_5
+        text_to_transform_6 += CAPS_LETTER_X_6
+    elif letter == "Y":
+        text_to_transform_1 += CAPS_LETTER_Y_1
+        text_to_transform_2 += CAPS_LETTER_Y_2
+        text_to_transform_3 += CAPS_LETTER_Y_3
+        text_to_transform_4 += CAPS_LETTER_Y_4
+        text_to_transform_5 += CAPS_LETTER_Y_5
+        text_to_transform_6 += CAPS_LETTER_Y_6
+    elif letter == "Z":
+        text_to_transform_1 += CAPS_LETTER_Z_1
+        text_to_transform_2 += CAPS_LETTER_Z_2
+        text_to_transform_3 += CAPS_LETTER_Z_3
+        text_to_transform_4 += CAPS_LETTER_Z_4
+        text_to_transform_5 += CAPS_LETTER_Z_5
+        text_to_transform_6 += CAPS_LETTER_Z_6
+    elif letter == " ":
+        text_to_transform_1 += "          "
+        text_to_transform_2 += "          "
+        text_to_transform_3 += "          "
+        text_to_transform_4 += "          "
+        text_to_transform_5 += "          "
+        text_to_transform_6 += "          "
+    else:
+        text_to_transform_1 += ""
+        text_to_transform_2 += ""
+        text_to_transform_3 += ""
+        text_to_transform_4 += ""
+        text_to_transform_5 += ""
+        text_to_transform_6 += ""
+
+transformed_text = (
+    text_to_transform_1
+    + "\n"
+    + text_to_transform_2
+    + "\n"
+    + text_to_transform_3
+    + "\n"
+    + text_to_transform_4
+    + "\n"
+    + text_to_transform_5
+    + "\n"
+    + text_to_transform_6
+)
+
+print(transformed_text)
